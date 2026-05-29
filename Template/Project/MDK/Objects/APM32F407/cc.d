e823eea4734ba3ac1e2184c07be7de0c60195358
BaiChuCanShuang <2760196608@qq.com>
2026-05-30 01:11:00 +0800

    Revert "apm32"
    This reverts commit 0079e1297089662031b59a656f283713f29c417f.

--- Template/Project/MDK/Objects/APM32F407/cc.d
+++
@@ -1,6 +0,0 @@
-.\objects\apm32f407\cc.o: SOES\src\cc.c
-.\objects\apm32f407\cc.o: .\SOES\inc\cc.h
-.\objects\apm32f407\cc.o: D:\keil5\ARM\ARMCC\Bin\..\include\assert.h
-.\objects\apm32f407\cc.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\apm32f407\cc.o: D:\keil5\ARM\ARMCC\Bin\..\include\stddef.h
-.\objects\apm32f407\cc.o: D:\keil5\ARM\ARMCC\Bin\..\include\inttypes.h
--- Template/Project/MDK/Objects/APM32F407/lan9252_hal.d
+++
@@ -1,5 +0,0 @@
-.\objects\apm32f407\lan9252_hal.o: Drivers\lan9252_hal.c
-.\objects\apm32f407\lan9252_hal.o: Drivers\lan9252.h
-.\objects\apm32f407\lan9252_hal.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\apm32f407\lan9252_hal.o: Drivers\fsmc_drv.h
-.\objects\apm32f407\lan9252_hal.o: D:\keil5\ARM\ARMCC\Bin\..\include\string.h
